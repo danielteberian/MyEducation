5b19c12483c74b383a82df7ff8aeb9299f752bf3
danielteberian <danielteberian@gmail.com>
2021-05-19 15:42:11 -0500

    Added two scripts: a Python version checker, and the obligatory HELLO WORLD script.

---
+++ Notes/Python-3/Scripts/hello_world.py
@@ -0,0 +1,4 @@
+import sys
+#I have no idea if I even need to import anything, but sys is one of those things that cannot really harm my code (at least, as far as I know).
+
+print ("Wassup, earth?")
